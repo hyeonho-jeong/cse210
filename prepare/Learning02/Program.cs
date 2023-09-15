13600dd5d131935b283008dd154723e58df31e60
Hyeonho. Joeng <jeo16003@byui.edu>
2023-09-14 19:05:39 -0600

    Prep2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.Write("Enter number: ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+        int y = 2;
+        int z = 5;
+
+        if((x<y || x<z) && y<z)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if(x < y)
+        {
+            Console.WriteLine("Less");
+        }
+
+        else
+        {
+            Console.WriteLine("Equal");
+        }
     }
 }
