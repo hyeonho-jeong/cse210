b21d7c9b496c3fdacc965c1e522b2d5879d63159
Hyeonho. Joeng <jeo16003@byui.edu>
2023-09-14 18:44:53 -0600

    prep1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello sandbox World!!!");
+        Console.WriteLine("this is in C#");
+
+        int number = 5;
+        number = 8;
+        number =number+3;
+
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();//input 이라 보면 됨
+        Console.WriteLine("Your color is" + color);//print 라 보면 됨
+    //Console.WriteLine($"Your color is {color}"); 위의 값과 같음
     }
 }
