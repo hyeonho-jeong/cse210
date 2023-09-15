f0e764042827cc073a69f3b5a9035d35e8d82f0d
Hyeonho. Joeng <jeo16003@byui.edu>
2023-09-14 19:17:26 -0600

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        //This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score? ");
+        string score = Console.ReadLine();
+        int grade = int.Parse(score);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congraturation!");
+        }
+        else
+        {
+            Console.WriteLine("You can make it next time!");
+        }
     }
 }
