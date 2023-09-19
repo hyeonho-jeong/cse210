b7d03eec2c39184638b34233d305bb409e1480fe
Hyeonho. Joeng <jeo16003@byui.edu>
2023-09-19 12:52:20 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Randsom();
+        int number = randomGenerator.Next(1,100);
+
+        Console.Write("What is your magic number?");
+        int guess = Console.ReadLine();
+
+        if(guess != number)
+        {
+            while(guess == number)
+            {
+                if(guess > number)
+                {
+                    Console.Write('Lower');
+                }
+                else
+                {
+                    Console.Write('Higher');
+                }
+                Console.Write("What is your magic number?");
+                int guess = Console.ReadLine();
+
+            }
+        }
+
+        else
+        {
+            Console.WriteLine('You guessed it!')
+        }
     }
 }
