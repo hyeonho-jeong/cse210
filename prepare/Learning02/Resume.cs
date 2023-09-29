1c507a96d48267df144d1f64d5eab8949560f771
Hyeonho. Joeng <jeo16003@byui.edu>
2023-09-29 16:02:20 -0600

    study

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -5,6 +5,10 @@ public class Resume
     public string _personName;
     public List<Job> _jobs = new List<Job>();
 
+    public Resume()
+    {
+    }
+
     public void Display()
     {
         Console.WriteLine($"Name: {_npersonName}");
