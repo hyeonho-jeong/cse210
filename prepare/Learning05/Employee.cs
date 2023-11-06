abed82dab47e5b02553ba2839d885eeba3416751
hyeonho <hyeonhoj0792@gmail.com>
2023-11-06 16:44:14 -0700

    change

---
+++ prepare/Learning05/Employee.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Employee
+{
+    protected string _name;
+    protected string _idNumber;
+
+    public Employee()
+    {
+
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public string GetIdNumber()
+    {
+        return _idNumber;
+    }
+
+    public void SetIdNumber(string idNumber)
+    {
+        _idNumber = idNumber;
+    }
+
+    public virtual float GetPay()
+    {
+        return -1;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using employee;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
+        HourlyEmployee hEmployee = new HourlyEmployee();
+        hEmployee.SetName("John");
+        hEmployee.SetIdNumber("123ASD");
+        hEmployee.SetPayRate(15);
+        hEmployee.SetHoursWorked(35);
+
+        SalaryEmployee sEmployee = new SalaryEmployee();
+        sEmployee.SetName("Peter");
+        sEmployee.SetIdNumber("456dsf");
+        sEmployee.SetSalary(6000);
+
+        DisplayEmployeeInformation(hEmployee);
+        DisplayEmployeeInformation(sEmployee);
+
+        List<Employee> employees = new List<Employee>();
+        employees.Add(hEmployee);
+        employees.Add(sEmployee);
+
+        foreach(Employee emp in employees)
+        {
+            float pay = emp.GetPay();
+        }
+    }
+
+    public static void DisplayEmployeeInformation(Employee employee)
+    {
+      
+
+        float pay = employee.GetPay();    
+        Console.WriteLine($"{employee.GetName()} will be paied ${pay}");
     }
 }
